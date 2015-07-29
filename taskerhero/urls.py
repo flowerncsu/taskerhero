128b12e73b276f447816f8c6b8f020f490d855dd
Charlotte Mays <charlotte.ann.mays@gmail.com>
2015-07-29 14:22:50 -0400

    login and logout work (TODO: there are no links to them and 'log in again' after logout goes to the wrong login page)

--- taskerhero/urls.py
+++ taskerhero/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout),
 ]
