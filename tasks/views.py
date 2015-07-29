a5ce325499bb7c67ec31834deebb86caaf5cfa49
Charlotte Mays <charlotte.ann.mays@gmail.com>
2015-07-29 17:36:49 -0400

    'tasks/all' page now displays all of the logged-in user's tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Task
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def all(request):
-    return render(request, 'tasks/all.html')
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/all.html', {'tasks': tasks})
