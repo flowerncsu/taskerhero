2b4a5b2c6ebf1a07fdc7b82aa089742784b7ce8b
Charlotte Mays <charlotte.ann.mays@gmail.com>
2015-07-30 12:17:57 -0400

    added interactivity to task page; tasks can be added and marked as complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -18,4 +18,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.all),
+    url(r'^test$', views.test),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from .models import Task
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.utils import timezone
+
+def update_tasks(request):
+    for pk in request.POST.getlist('completed'):
+        task = Task.objects.get(pk=pk)
+        task.completed = True
+        task.save()
+    if ('new_task' in request.POST) and (request.POST['new_task'] != ''):
+        task = Task(task_name = request.POST['new_task'],
+                    create_date = timezone.now(),
+                    user=request.user)
+        task.save()
 
 @login_required
 def all(request):
-    tasks = Task.objects.filter(user=request.user)
+    update_tasks(request)
+    tasks = Task.objects.filter(user=request.user, completed=False)
     return render(request, 'tasks/all.html', {'tasks': tasks})
+
+def test(request):
+    return HttpResponse(request.POST.getlist('completed'))
