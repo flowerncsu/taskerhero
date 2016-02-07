8eb9d14ff1bf0572babc7727e16d5806d75f949d
Charlotte Mays <charlotte.ann.mays@gmail.com>
2016-02-07 14:10:08 -0500

    Changed "repeating" boolean field to be an enum

--- tasks/models.py
+++ tasks/models.py
@@ -11,12 +11,27 @@ class Task(models.Model):
     completed_date = models.DateField(null=True, blank=True)
     for_today = models.BooleanField(default=False)
     due_date = models.DateField(null=True, blank=True)
-    repeating = models.BooleanField(default=False)
-    # set repeat interval
-    # set repeat type
-    #   - interval-based, ie: every x days or x days after last completion
-    #   - calendar-based, ie: every week on Monday
+    # repeat type
+    #   - interval_after: repeats x days after last completion
+    #       - If this is chosen, repeat_weeks will not be used.
+    #   - interval_every: repeats x days after last instantiation
+    #       - If this is chosen, repeat_weeks will not be used.
+    #   - TO BE IMPLEMENTED: calendar, ie: every week on Monday
+    #       - repeat_weeks is how many weeks between instances
+    #       - repeat_days is which day of the week to instantiate
+    #           - 0=Sunday, 1=Monday, ..., 6=Saturday
     # set priority levels
+    NON_REPEATING = 'NR'
+    INTERVAL_AFTER = 'IA'
+    INTERVAL_EVERY = 'IE'
+    repeat_type = models.CharField(max_length=2,
+                                   choices=((NON_REPEATING, 'No Repeat'),
+                                            (INTERVAL_AFTER, 'Repeat After X Days'),
+                                            (INTERVAL_EVERY, 'Repeat Every X Days'), )
+                                   default=NON_REPEATING)
+    repeat_days = models.IntegerField('days between instances', null=True, blank=True)
+    repeat_weeks = models.IntegerField('weeks between instances', null=True, blank=True)
+    next_due_date = models.DateField('for repeating tasks', null=True, blank=True)
     def __str__(self):
         return self.task_name
     # xp should be calculated at task completion so that its age is taken
