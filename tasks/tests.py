093743b37a993823141c9ecd3c85f2968a676314
Charlotte Mays <charlotte.ann.mays@gmail.com>
2016-11-01 18:26:25 -0400

    one functioning test... it's a start!

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,25 @@
+from .models import Task
+from django.contrib.auth.models import User
+from userprofile.models import UserProfile
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class TaskTestSuite(TestCase):
+    def setUp(self):
+        self.user1 = User(username='user1', email='test@example.com', password='abc123')
+        self.user2 = User(username='user2', email='hi@hello.com', password='123abc')
+        self.user1.save()
+        self.user2.save()
+        user1profile = UserProfile(user=self.user1)
+        user2profile = UserProfile(user=self.user2)
+        user1profile.save()
+        user2profile.save()
+
+    def test_task_creation(self):
+        user1_task = Task(user=self.user1, task_name="user1's task")
+        user1_task.save()
+        self.assertEqual(len(Task.objects.filter(user=self.user1)), 1)
+
+    def test_repeating_task(self):
+        pass
